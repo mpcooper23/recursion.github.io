9e1c4f7bc2f714949f49b2bdddb2bd21a28ea37b
mpcooper23 <mpcooper23@gmail.com>
2024-12-18 11:48:35 -0600

    1

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -5,6 +5,9 @@
 // Example:  5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5);  // 120
 var factorial = function(n) {
+  if (n === 1){
+    return 1
+  }else return n * factorial(n -1)
 };
 
 // 2. Compute the sum of an array of integers.
