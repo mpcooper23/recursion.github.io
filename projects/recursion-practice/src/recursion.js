025a571f2d177b5ac05c23c6c5d505cb00157082
mpcooper23 <mpcooper23@gmail.com>
2025-01-10 14:26:47 -0600

    1

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -4,10 +4,12 @@
 // denoted by n!, is the product of all positive integers less than or equal to n.
 // Example:  5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5);  // 120
-var factorial = function(n) {
+var factorial = n => {
   if (n === 1){
     return 1
-  }else return n * factorial(n -1)
+  }else {
+    return n * factorial(n -1);
+  }
 };
 
 // 2. Compute the sum of an array of integers.
